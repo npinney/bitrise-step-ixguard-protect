76e4324e7261ff675f292ec615cb18610f708f25
Nicholas Pinney <npinney@petalmd.com>
2024-11-12 15:50:07 -0500

    Added logic

--- step.sh
+++ step.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+LICENSE_FILE="ixguard-license.txt"
+EXPORT_OPTIONS_PLIST="export_options.plist"
+PROTECTED_ARCHIVE="protected.xcarchive"
+IXGUARD_GENERATED_FILES_DIRECTORY="ixguard-files"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Download the license file
+if [ -n "${BITRISEIO_IXGUARD_LICENSE_URL}" ]; then
+    curl ${BITRISEIO_IXGUARD_LICENSE_URL} -o "$LICENSE_FILE"
+else
+    echo "Need to import the ixguard license file into Bitrise."
+    exit 1
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+# Download the export options plist file
+if [ -n "${BITRISEIO_EXPORT_OPTIONS_URL}" ]; then
+    curl ${BITRISEIO_EXPORT_OPTIONS_URL} -o "$EXPORT_OPTIONS_PLIST"
+else
+    echo "Need to import the export options file into Bitrise."
+    exit 1
+fi
+
+# Make sure ixguard is installed
+if ! command -v ixguard >/dev/null 2>&1; then
+    echo "iXGuard must be installed."
+    exit 1
+fi
+
+# Process the existing xcarchive
+ixguard -config "ixguard.yml" -o="$PROTECTED_ARCHIVE" "$BITRISE_XCARCHIVE_PATH"
+
+envman add --key PROTECTED_ARCHIVE --value "$(realpath $PROTECTED_ARCHIVE)"
+
+# Create the output directory
+mkdir "$IXGUARD_GENERATED_FILES_DIRECTORY"
+
+# Group and compress the generated files
+mv "ixguard-dsymutil.log" "ixguard.log" "mapping.yml" "protectionreport.html" "statistics.yml" "support_files.zip" "telemetry_dump.json" "$IXGUARD_GENERATED_FILES_DIRECTORY/"
+cp -r "$PROTECTED_ARCHIVE" "$IXGUARD_GENERATED_FILES_DIRECTORY/"
+zip -r "$IXGUARD_GENERATED_FILES_DIRECTORY.zip" "$IXGUARD_GENERATED_FILES_DIRECTORY"
+
+mv "$IXGUARD_GENERATED_FILES_DIRECTORY.zip" "$BITRISE_DEPLOY_DIR/"
+exit 0
